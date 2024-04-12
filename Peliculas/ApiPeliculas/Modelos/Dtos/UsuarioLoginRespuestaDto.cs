3748e3739a5cebea1e404e48b21d618f9a4892cf
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-04-12 16:29:10 -0500

    Borrado

--- Peliculas/ApiPeliculas/Modelos/Dtos/UsuarioLoginRespuestaDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ApiPeliculas.Modelos.Dtos
-{
-    public class UsuarioLoginRespuestaDto
-    {
-        public UsuariosDatosDto Usuario { get; set; }
-        public string Token { get; set; }
-    }
-}
