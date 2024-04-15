39c2c93287fd517bae1e6f3025408b658967b537
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-04-14 21:03:50 -0500

    Comienzo del Proyecto

---
+++ Peliculas/Peliculas/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Peliculas.Entities;
+
+namespace Peliculas
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options) { }
+
+        public DbSet<Genero> Generos { get; set; }
+    }
+}
---
+++ Peliculas/Peliculas/Entities/Genero.cs
@@ -0,0 +1,8 @@
+﻿namespace Peliculas.Entities
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
